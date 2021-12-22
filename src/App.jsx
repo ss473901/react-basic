ba2cbf5306a4ed098d08c55b097bc33739f68552
ss473901 <satoshi.473901.1205@gmail.com>
2021-12-22 12:58:58 +0900

    base component

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import defaultDataset from './detaset';
+import './assets/styles/style.css';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+     this.state = {
+       answers: [],         //回答コンポーネントに表示するデータ
+       chats: [],           //チャットコンポーネントに表示するデータ
+       currentId: "init",  //現在の質問ID
+       dataset: defaultDataset,　　    //質問と回答のデータセット
+       open: false        //問い合わせフォーム用モーダルの開閉を管理
+     }
+  }
+  render(){
+    return (
+      <div>
+        <section className="c-section">
+          <div className="c-box">
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
